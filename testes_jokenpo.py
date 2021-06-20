dcc86e579e30a580cc8972a91c99cbc38fd126cf
Guilherme Mattoso <guilhermemktfran@gmail.com>
2021-06-19 22:38:22 -0300

    Atualização

--- testes_jokenpo.py
+++ testes_jokenpo.py
@@ -1,5 +1,6 @@
 from time import sleep
 from random import randint
+sleep(1)
 print('''
                                                              JO KEN PO
 
@@ -11,22 +12,22 @@ print('''
     Eles eram chefes de tribos diferentes que estavam lutando para tomar o controle do território, mas não queriam uma guerra, pois
     sabiam que iriam derramar muito sangue inocente e como haviam se tornado detentores de um enorme poder lhes veio um grande senso de responsabilidade.
          Então decidiram realizar um torneio entre eles, quem vencesse iria se torna Rei de todo o território e os perdedores iriam ter 
-    dois caminhos a seguir ou se jutarão ao novo rei e o apoirão ou levarão toda a sua tribo para outras terras...
-    
-''')
-regras='''- O torneio de Jo, Ken e Pô está preste a começar, mas antes vamos as regras:
+    dois caminhos a seguir ou se jutarão ao novo rei e o apoirão ou levarão toda a sua tribo para outras terras...    ''')
+sleep(10)
+print()
+print('''Ancião:
+- O torneio de Jo, Ken e Pô está preste a começar, mas antes vamos as regras:
    1º Você deve escolher quantos rounds quer batalhar.
    2º Escolha um guerreiro, lembrando que:
    -Jo, representa o poder Terra;
    -Ken, representa o poder Metal;
    -Po, representa o poder folha. 
-   3º O ancião irá escolher o seu openente para lutar, atravès do sorteio da urna sagrada.
-   4º Se divirta! '''
-   
+   3º Eu irá escolher o seu openente para lutar, atravès do sorteio da urna sagrada.
+   4º Se divirta! ''')
+print()
 pergunta=''
 rounds=0
 rodadas_ganhas=rodadas_peridas=empates=0
-print(regras)
 while True:
    rounds=int(input('Quantos rounds você quer jogar? '))
    guerreiro=str(input('Qual guerreiro é você? ')).upper().strip()
@@ -47,36 +48,49 @@ if guerreiro in "JO,KEN,PO":
 
      for n in range(rounds):
           npc=randint(1,3) 
+          sleep(2)
           print(f'\n{n+1}º rodada')                
           if npc == 1 and guerreiro==2:
                if guerreiro>npc:
-                    print('vc ganha')
+                    print('Ancião:\n- A urna sagrada decidiu, seu oponente será Jo!\n')
+                    sleep(3)
+                    print('Juiz:\n- Ken o sevéro ganhou a batalha!')
                     rodadas_ganhas+=1
           if npc == 1 and guerreiro == 3:
                if guerreiro>npc:
-                    print('vc perdeu')
+                    print('Ancião:\n- A urna sagrada decidiu, seu openente será Jo!\n')
+                    sleep(3)
+                    print('Juiz:\n- Po o justo, perdeu a batalha!')
                     rodadas_peridas+=1
           if npc == 2 and guerreiro == 1:
                if npc>guerreiro:
-                    print('vc perdeu')
+                    print('\nAncião:\n- A urna sagrada decidiu, seu oponente será Ken!\n')
+                    sleep(3)
+                    print('Juiz:\n- Jo o destemido, perdeu a batalha!')
                     rodadas_peridas+=1            
           if npc == 2 and guerreiro == 3:
                if npc<guerreiro:
-                    print('vc ganha') 
+                    print('\nAncião:\n- A urna sagrada decidiu, seu oponente será Ken!\n')
+                    sleep(3)
+                    print('Juiz:\n- Po o just ganhou a batalha!')
                     rodadas_ganhas+=1   
           if npc == 3 and guerreiro == 1:
                if npc>guerreiro:
-                    print('vc ganha')
+                    print('\nAncião:\n- A urna sagra decidiu, seu oponente será Po!\n')
+                    sleep(3)
+                    print('Juiz:\n- Jo o destemido ganhou a batalha!')
                     rodadas_ganhas+=1
           if npc == 3 and guerreiro == 2:
                if npc>guerreiro:
-                    print('vc perdeu')
+                    print('\nAncião:\n- A urna sagrada decidiu, seu oponente será Po!\n')
+                    sleep(3)
+                    print('Juiz:\n- Ken o sevéro perdeu a batalha!')
                     rodadas_peridas+=1
           if npc==guerreiro:
                print('empate')
                empates+=1
           sleep(1)
-print()
-print(f'empates{empates}')
-print(f'ganhou {rodadas_ganhas}')
-print(f'perdeu {rodadas_peridas}')
+     print()
+     print(f'empates{empates}')
+     print(f'ganhou {rodadas_ganhas}')
+     print(f'perdeu {rodadas_peridas}')
